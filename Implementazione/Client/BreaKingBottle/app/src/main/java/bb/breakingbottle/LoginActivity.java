80818fc5a029a8d4d8ae20cdb832446657fc44e9
sumezzu <nicola.mazzara@studenti.unipr.it>
2016-01-28 10:30:59 +0100

    Client con activity minimamente pronto per testare server

--- Implementazione/Client/BreaKingBottle/app/src/main/java/bb/breakingbottle/LoginActivity.java
+++ Implementazione/Client/BreaKingBottle/app/src/main/java/bb/breakingbottle/LoginActivity.java
@@ -3,7 +3,12 @@ package bb.breakingbottle;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.app.Fragment;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -27,12 +32,16 @@ import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static android.Manifest.permission.READ_CONTACTS;
+import static bb.breakingbottle.R.color.colorAccent;
+import static bb.breakingbottle.R.color.colorPrimaryDark;
+
 
 /**
  * A login screen that offers login via email/password.
@@ -42,7 +51,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     /**
      * Id to identity READ_CONTACTS permission request.
      */
+
     private static final int REQUEST_READ_CONTACTS = 0;
+    private boolean b = false;
 
     /**
      * A dummy authentication store containing known user names and passwords.
@@ -82,13 +93,47 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
         });
 
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        final TextView creaAccount = (TextView) findViewById(R.id.crea_account);
+        creaAccount.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                creaAccount.setSelected(!creaAccount.isPressed());
+                if (creaAccount.isPressed()) {
+                    creaAccount.setTextColor(getResources().getColorStateList(R.color.textcolor));
+                    Intent intent = new Intent(LoginActivity.this,SignIn.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
+        final ImageButton mEmailSignInButton = (ImageButton) findViewById(R.id.email_sign_in_button);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
+                mEmailSignInButton.setSelected(!mEmailSignInButton.isPressed());
+                if(!b) {
+                    b = !b;
+                    if (mEmailSignInButton.isPressed()) {
+                        mEmailSignInButton.setImageResource(R.drawable.bottlebrokebottomlogin);
+                        Intent intent = new Intent(LoginActivity.this,SceltaPersonaggio.class);
+                        startActivity(intent);
+                    } else {
+                        mEmailSignInButton.setImageResource(R.drawable.bottlebottomlogin);
+                    }
+                }
+                else {
+                    b = !b;
+                    if (mEmailSignInButton.isPressed()) {
+                        mEmailSignInButton.setImageResource(R.drawable.bottlebottomlogin);
+                    } else {
+                        mEmailSignInButton.setImageResource(R.drawable.bottlebrokebottomlogin);
+                    }
+                }
+
                 attemptLogin();
             }
         });
+
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
